2024238290d1a64c2c0af6822bb576f7dc7c2e30
Steffen <steffen.kohn@web.de>
2020-07-01 11:43:29 +0200

    Test with bezier curves - unsuccessful
    Inbuild bezier curve seems to be not the right approach 
    + Fixed bottle scale and rotation

---
+++ Assets/DrinkPouring/BezierExample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BezierExample : MonoBehaviour
+{
+    public Texture2D bezierTexture;
+    public Transform bottleOpening;
+
+    public Vector3 startPoint = new Vector3(-0.0f, 0.0f, 0.0f);
+    public Vector3 endPoint = new Vector3(-2.0f, 2.0f, 0.0f);
+    public Vector3 startTangent = Vector3.zero;
+    public Vector3 endTangent = Vector3.zero;
+    private float elapsed = 0f;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
+        {
+            elapsed = elapsed % 0.3f;
+            startPoint = bottleOpening.position;
+        }
+    }
+}
