7d083c1013e1932474e776a71e79c3a92fb0d0a7
Steffen <steffen.kohn@web.de>
2020-07-01 17:09:35 +0200

    Pour Effect with LineRenderer

---
+++ Assets/DrinkPouring/PourDetector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentSteam = null;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        print("Start");
+        currentSteam = CreateStream();
+        currentSteam.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("End");
+        currentSteam.End();
+        currentSteam = null;
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.up.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+}
