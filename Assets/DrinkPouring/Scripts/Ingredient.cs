dea83d2411ff17967d569b340f0e920c53eaacfc
Steffen <steffen.kohn@web.de>
2020-07-02 12:37:46 +0200

    Imported the drink mixing logic from the first project repo

---
+++ Assets/DrinkPouring/Scripts/Ingredient.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Ingredient
+{
+    public abstract Enum Type
+    {
+        get;
+    }
+}
+
+public enum SolidType
+{
+    Ice,
+    LemonSlice,
+    Mint,
+    Lime,
+    Sugar,
+    Cherry,
+    OrangeSlice
+}
+
+public enum LiquidType
+{
+    Cola,
+    Rum,
+    Vodka,
+    Gin,
+    TonicWater,
+    Tequila,
+    OrangeJuice,
+    Grenadine,
+    Cachaca,
+    LimeJuice,
+    SimpleSyrup,
+    Soda,
+    TripleSec,
+    LemonJuice,
+    GommeSyrup,
+    CranberryJuice,
+    PineappleJuice,
+    CoconutCream,
+    Cream,
+    Vermouth,
+    Angostura,
+    Cointreau,
+    Whiskey
+}
+
+public class IngredientComparer<Ingredient> : IEqualityComparer<Ingredient>
+{
+    public bool Equals(Ingredient x, Ingredient y)
+    {
+        return x.Equals(y);
+    }
+
+    public int GetHashCode(Ingredient obj)
+    {
+        return 0;
+    }
+}
+
+public class SolidIngredient : Ingredient
+{
+    private SolidType type;
+
+    public SolidIngredient(SolidType type)
+    {
+        this.type = type;
+    }
+
+    public override Enum Type
+    {
+        get
+        {
+            return this.type;
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is SolidIngredient)
+        {
+            return this.type == ((SolidIngredient)obj).type;
+        }
+        return base.Equals(obj);
+    }
+}
+
+public class LiquidIngredient : Ingredient
+{
+    private LiquidType type;
+
+    public LiquidIngredient(LiquidType type)
+    {
+        this.type = type;
+    }
+
+    public override Enum Type
+    {
+        get
+        {
+            return this.type;
+        }
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is LiquidIngredient)
+        {
+            return this.type == ((LiquidIngredient)obj).type;
+        }
+        return base.Equals(obj);
+    }
+}
---
+++ Assets/DrinkPouring/Scripts/Shaker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    private Dictionary<Ingredient, int> ingrediens;
+    //private LiquorLibrary liquorLibrary;
+
+    private void Start()
+    {
+        //liquorLibrary = new LiquorLibrary();
+        IngredientComparer<Ingredient> ingredientComparer = new IngredientComparer<Ingredient>();
+        ingrediens = new Dictionary<Ingredient, int>(ingredientComparer);
+
+        //Ingredient ice = new SolidIngredient(SolidType.Ice);
+        //Ingredient ice2 = new SolidIngredient(SolidType.Ice);
+        //Ingredient water = new LiquidIngredient(LiquidType.Cola);
+
+        addIngredient(new SolidIngredient(SolidType.Ice), 1);
+        addIngredient(new SolidIngredient(SolidType.Lime), 1);
+        addIngredient(new SolidIngredient(SolidType.Sugar), 1);
+        addIngredient(new SolidIngredient(SolidType.Sugar), 1);
+        addIngredient(new LiquidIngredient(LiquidType.Cachaca), 50);
+
+        mixDrink();
+    }
+
+    public void addIngredient(Ingredient ingredient, int quantity)
+    {
+        int value = 0;
+
+        if (ingrediens.TryGetValue(ingredient, out value)) // Fügt einem gespeicherten Ingredient etwas hinzu oder legt es an
+        {
+            ingrediens[ingredient] += quantity;
+        }
+        else
+        {
+            ingrediens.Add(ingredient, quantity);
+        }
+
+        Debug.Log(ingredient.Type + ": " + ingrediens[ingredient]);
+    }
+
+    public void mixDrink()
+    {
+        //Debug.Log("MixDrink");
+        // Search for a Recepie for the ingredients added
+        Drink newDrink = new Drink(DrinkType.Default, ingrediens);
+
+        //LiquorLibrary.discoverDrinkType(newDrink);
+        // return the Drink if it exists or default Drink
+        DrinkType drinkType = LiquorLibrary.discoverDrinkType(newDrink);
+        if (drinkType != DrinkType.Default)
+        {
+            Debug.Log("Success: " + drinkType);
+        }
+        else
+        {
+            Debug.Log("Failure: " + drinkType);
+        }
+
+        //return new Drink();
+    }
+}
