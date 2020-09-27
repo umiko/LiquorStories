09947b60f7539f21b85279330020bc578570fbcb
John Smith <umiko@protonmail.com>
2020-09-27 02:53:47 +0200

    Drinks can now be easily initialized by code on start

---
+++ Assets/Scripts/DrinkInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DrinkInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        /*
+        Get Drink Type
+        Set Labels
+        Set Liquid Color
+        */
+
+        PourDetector pd = GetComponent<PourDetector>();
+        LiquidType contents = pd.liqourType;
+        TextMeshPro[] labels = GetComponentsInChildren<TextMeshPro>();
+        foreach (TextMeshPro textMeshPro in labels)
+        {
+            textMeshPro.text = contents.ToString("G");
+        }
+        Color liquidColor = LiquidColour.getLiquidColor(contents);
+        Renderer liquidRenderer = gameObject.GetComponentInChildren<Wobble>().gameObject.GetComponent<Renderer>();
+        liquidRenderer.material.SetColor("_Tint", liquidColor);
+        liquidRenderer.material.SetColor("_TopColor", liquidColor + new Color(0.1f,0.1f,0.1f, 1 ));
+        liquidRenderer.material.SetColor("_FoamColor", liquidColor+ new Color(0.1f,0.1f,0.1f, 1 ));
+        var particleSystemMainModule = GetComponent<ParticleSystem>().main;
+        particleSystemMainModule.startColor = liquidColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
