085906f6cf8e611a537af9479936e55d975485ca
Steffen <steffen.kohn@web.de>
2020-09-28 20:21:35 +0200

    IngredientBox

---
+++ Assets/Scripts/IngredientBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientBox : MonoBehaviour
+{
+    public SolidType solidType;
+    public GameObject[] ingredients;
+    private GameObject objToSpawn;
+    public int spawnNum = 2;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        objToSpawn = ingredients[(int)solidType];
+        StartCoroutine("spawnNewIngredient");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "SolidIngredient")
+        {
+            spawnNum = 1;
+            StartCoroutine("spawnNewIngredient");
+            Destroy(other.gameObject, 20f);
+        }
+    }
+
+    private IEnumerator spawnNewIngredient()
+    {
+        if (objToSpawn)
+        {
+            for (int i = 0; i < spawnNum; i++)
+            {
+                Debug.Log(transform.position);
+                Instantiate(objToSpawn, transform.position + new Vector3(0, 0.2f, 0), Quaternion.identity);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        else
+        {
+            Debug.LogError("IngredientBox could not find Ingredient");
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/IngredientObj.cs
+++ Assets/Scripts/IngredientObj.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 public class IngredientObj : MonoBehaviour
 {
     public SolidType solidType;
-    private Shaker shaker;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        GameObject shakerOBJ = GameObject.Find("Shaker");
-        shaker = shakerOBJ.GetComponent<Shaker>();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.name == "Nozzle")
         {
-            Debug.Log("Attached  " + collision.collider.name);
-            shaker.addIngredient(new SolidIngredient(solidType), 1);
+            collision.collider.transform.parent.GetComponent<Shaker>().addIngredient(new SolidIngredient(solidType), 1);
+            Destroy(this);
         }
     }
 }
