3b2dcc633eab18501f2bf7c8e632cd9b10429bd7
Steffen <steffen.kohn@web.de>
2020-09-28 18:26:31 +0200

    Ingredientbox

---
+++ Assets/IngredientBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientBox : MonoBehaviour
+{
+    public SolidType solidTypes;
+    public int spawnNum = 12;
+    private SolidIngredient ingredient;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < spawnNum; i++)
+        {
+            Instantiate(, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.)
+    }
+}
---
+++ Assets/Scripts/IngredientObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientObj : MonoBehaviour
+{
+    public SolidType solidType;
+    private Shaker shaker;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject shakerOBJ = GameObject.Find("Shaker");
+        shaker = shakerOBJ.GetComponent<Shaker>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Nozzle")
+        {
+            Debug.Log("Attached  " + collision.collider.name);
+            shaker.addIngredient(new SolidIngredient(solidType), 1);
+        }
+    }
+}
