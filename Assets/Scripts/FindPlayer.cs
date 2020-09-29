2db4865504b90e9a7d8c713513b8e5c64175fbb1
John Smith <umiko@protonmail.com>
2020-09-29 03:17:36 +0200

    Finished the ability to restart the scene

---
+++ Assets/Scripts/FindPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+
+public class FindPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PlayerPrefab;
+    void Start()
+    {
+        
+        if (!Player.instance)
+        {
+            Instantiate(PlayerPrefab);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        Player.instance.transform.position = transform.position;
+        Player.instance.transform.rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
